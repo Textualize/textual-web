b7046bcb92509d6b3fd6f31aa1ebf1d29102cbc3
Will McGugan <willmcgugan@gmail.com>
2023-08-22 13:11:56 +0100

    slugify

---
+++ src/textual_web/apps/welcome.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from textual.app import App, ComposeResult
+from textual.widgets import Digits, Label
+
+
+class WelcomeApp(App):
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    #clock {
+        width: auto;
+    }
+    Digits {
+        color: $success;        
+    }
+    Label {
+        text-align: center;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Label("Welcome to textual-web!")
+        yield Digits("", id="clock")
+
+    def on_ready(self) -> None:
+        self.update_clock()
+        self.set_interval(1, self.update_clock)
+
+    def update_clock(self) -> None:
+        clock = datetime.now().time()
+        self.query_one(Digits).update(f"{clock:%T}")
+
+
+if __name__ == "__main__":
+    app = WelcomeApp()
+    app.run()
---
+++ src/textual_web/slugify.py
@@ -0,0 +1,22 @@
+import re
+import unicodedata
+
+
+def slugify(value: str, allow_unicode=False) -> str:
+    """
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")

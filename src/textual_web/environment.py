8b3ecbe8c93a7dfc750119d49573899effb563a3
Will McGugan <willmcgugan@gmail.com>
2023-08-22 11:16:14 +0100

    fix url

--- src/textual_web/environment.py
+++ src/textual_web/environment.py
@@ -24,7 +24,7 @@ ENVIRONMENTS = {
     ),
     "dev": Environment(
         name="dev",
-        api_url="https://textualize-dev.io/api",
+        api_url="https://textualize-dev.io/api/",
         url="wss://textualize-dev.io/app-service/",
     ),
 }
